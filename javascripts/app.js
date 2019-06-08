c08b34687cdda0b5b09e5477b1acc613e1a3245f
danyamashita <dan.yamashita@gmail.com>
2019-06-08 10:31:56 -0300

    initial commit

---
+++ javascripts/app.js
@@ -0,0 +1,177 @@
+// Rover Object Goes Here
+ var Rover = {
+   direction: "N",
+   x: 0,
+   y: 0,
+   travelLog:[[0,0]]
+ }
+// ======================
+
+let field = [[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+[null, null, null, null, null, null, null, null, null, null],
+]
+const obstableNb = 3 //Number of random obstacles//
+const fieldSize = 10
+const commands = 'rffrfflfrff'
+
+console.log(`random number`, getRandomNum())
+randomObs(obstableNb, fieldSize)
+validation(commands)
+
+// ======================
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+  switch(Rover.direction){
+    case "N":
+      Rover.direction="W"
+      break;
+    case "W":
+      Rover.direction="S"
+      break;
+    case "S":
+      Rover.direction="E"
+      break;
+    case "E":
+      Rover.direction="N"
+  }
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+  switch(Rover.direction){
+    case "N":
+      Rover.direction="E"
+      break;
+    case "E":
+      Rover.direction="S"
+      break;
+    case "S":
+      Rover.direction="W"
+      break;
+    case "W":
+      Rover.direction="N"
+  }
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+  switch(Rover.direction){
+    case "N":
+      Rover.y--
+      break;
+    case "E":
+      Rover.x++
+      break;
+    case "S":
+      Rover.y++
+      break;
+    case "W":
+      Rover.x--
+      break;
+  }
+  if (Rover.x<0){
+    Rover.x=0
+  }
+  if (Rover.x>10){
+    Rover.x=10
+  }
+  if(Rover.y<0){
+    Rover.y=0
+  }
+  if(Rover.y>10){
+    Rover.y=10
+  }
+  console.log(Rover.x, Rover.y)
+}
+
+function moveBackward(rover){
+  console.log("moveBackward was called")
+  switch(Rover.direction){
+    case "N":
+      Rover.y++
+      break;
+    case "E":
+      Rover.x--
+      break;
+    case "S":
+      Rover.y--
+      break;
+    case "W":
+      Rover.x++
+      break;
+  }
+  if (Rover.x<0){
+    Rover.x=0
+  }
+  if (Rover.x>10){
+    Rover.x=10
+  }
+  if(Rover.y<0){
+    Rover.y=0
+  }
+  if(Rover.y>10){
+    Rover.y=10
+  }
+  console.log(Rover.x, Rover.y)
+}
+
+function movement(instruction){
+  for (let i=0; i<instruction.length; i++){
+    switch(instruction[i]){
+      case "f":
+        moveForward()
+        Rover.travelLog.push([Rover.x, Rover.y])
+        break;
+      case "b":
+        moveBackward()
+        Rover.travelLog.push([Rover.x, Rover.y])
+        break;
+      case "r": 
+        turnRight()
+        break;
+      case "l":
+        turnLeft()
+        break;
+    }
+
+  }
+  console.log(Rover.travelLog)
+}
+
+function validation(instruction){
+  let valid = true
+  for (let i=0; i<instruction.length; i++){
+    if(instruction[i] != 'f' && instruction[i] != 'b' && instruction[i] != 'r' && instruction[i] != 'l'){
+      console.log("Invalid input, only f, b, r, l allowed")
+      valid = false
+      break;
+    } 
+  }
+  if (valid === true){
+    movement(instruction)
+  }
+}
+
+function getRandomNum(){
+  return Math.floor(Math.random() * (fieldSize+1));
+}
+
+function randomObs(ObstacleQTY, size){
+  for (i=0; i<ObstacleQTY; i++){
+    let row = getRandomNum()
+    let column = getRandomNum()
+    field[row][column]='obs'
+  }
+}
+
+function verifyMovement(currentPos, nextMov){
+
+}
